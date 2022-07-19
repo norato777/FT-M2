7fac45246e33ecd33b3863e49271221e0f46d4f7
Edison <norato777@gmail.com>
2022-07-19 13:38:47 -0500

    homework-js updated

--- 11-Redux/hw-js/src/reducer/index.js
+++ 11-Redux/hw-js/src/reducer/index.js
@@ -1,15 +1,31 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO, INCREMENTO_IMPAR } = require("../action-types");
 
 const initialState = {
-  contador: 0
-}
+  contador: 0,
+};
 
 // Nuestro reducer que maneja nuestros dos casos de acción incremento y decremento.
-// Recibe el estado de nuestro store, junto con una action creada por nuestro action creator. 
+// Recibe el estado de nuestro store, junto con una action creada por nuestro action creator.
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
 function contador(state = initialState, action) {
-  
+  switch (action.type) {
+    case INCREMENTO: {
+      return { contador: state.contador + 1 };
+    }
+    case DECREMENTO: {
+      return { contador: state.contador - 1 };
+    }
+
+    case INCREMENTO_IMPAR: {
+      return state.contador % 2 === 1
+        ? { contador: state.contador + 1 }
+        : state;
+    }
+
+    default:
+      return state;
+  }
 }
 
 module.exports = contador;
